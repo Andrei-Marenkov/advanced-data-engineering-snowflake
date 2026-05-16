66a69989f82fdec117577ce26d8a48c7aae2af29
Andrei-Marenkov <mopda2001@gmail.com>
2026-05-15 23:20:08 -0300

    Give proper signatures to functions and remove stream "ON TABLE" clause

--- cleanup.sql
+++ cleanup.sql
@@ -6,14 +6,14 @@ DROP VIEW tasty_bytes.harmonized.daily_weather_v;
 
 DROP VIEW tasty_bytes.harmonized.windspeed_hamburg;
 
-DROP FUNCTION tasty_bytes.analytics.fahrenheit_to_celsius();
+DROP FUNCTION tasty_bytes.analytics.fahrenheit_to_celsius(number);
 
-DROP FUNCTION tasty_bytes.analytics.inch_to_millimeter();
+DROP FUNCTION tasty_bytes.analytics.inch_to_millimeter(number);
 
 DROP VIEW tasty_bytes.harmonized.weather_hamburg;
 
 DROP VIEW tasty_bytes.analytics.daily_city_metrics_v;
 
-DROP STREAM tasty_bytes.raw_pos.order_header_stream ON TABLE tasty_bytes.raw_pos.order_header;
+DROP STREAM tasty_bytes.raw_pos.order_header_stream;
 
 DROP PROCEDURE tasty_bytes.raw_pos.process_order_headers_stream();
